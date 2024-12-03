f4fbce4394965e5578ad2b0405537b90751e7daa
Your Name <you@example.com>
2024-12-03 21:39:17 +0700

    add: edit profile section

--- UltiGear_BE/src/routes/orderRoutes.js
+++ UltiGear_BE/src/routes/orderRoutes.js
@@ -7,7 +7,7 @@ const authorization = require('../middleware/authorization');
 orderRoutes.post('/orders', auth, authorization('CUSTOMER'), orderController.createOrder); 
 orderRoutes.get('/orders', auth, authorization('CUSTOMER'), orderController.getAllOrders);  
 orderRoutes.get('/orders/:id', auth, authorization('CUSTOMER'), orderController.getOrderById); 
-orderRoutes.put('/orders/:id/payment-status', auth, authorization('CUSTOMER'), orderController.updateOrderPaymentStatus);  //masih eror
+orderRoutes.put('/orders/:id', auth, authorization('CUSTOMER'), orderController.updateOrderPaymentStatus);  
 orderRoutes.delete('/orders/:id', auth, authorization('CUSTOMER'), orderController.deleteOrder);  
 
 module.exports = orderRoutes;
