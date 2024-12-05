bfdfad0cc9847879f85faa856670530324333bcd
Your Name <you@example.com>
2024-12-06 01:33:06 +0700

    add: adminSection

--- UltiGear_FE/src/App.jsx
+++ UltiGear_FE/src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import HomePage from './pages/HomePage';
-import Regist from './pages/Regist';
-import Cart from './pages/Cart';
-import MyOrders from './pages/MyOrders';
-import EditProfile from './pages/EditProfile';
-import NotFound from './pages/NotFound';
-import Product from './pages/Product';
-import PaymentMethod from './pages/PaymentMethod';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import HomePage from "./pages/HomePage";
+import Regist from "./pages/Regist";
+import Cart from "./pages/Cart";
+import MyOrders from "./pages/MyOrders";
+import EditProfile from "./pages/EditProfile";
+import NotFound from "./pages/NotFound";
+import Product from "./pages/Product";
+import PaymentMethod from "./pages/PaymentMethod";
+import Dashboard from "./pages/adminSection/Dashboard"; 
 
 function App() {
   return (
@@ -18,10 +19,11 @@ function App() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/myorders" element={<MyOrders />} />
-        <Route path="/EditProfile" element={<EditProfile />} />
+        <Route path="/editprofile" element={<EditProfile />} />
         <Route path="/notfound" element={<NotFound />} />
-        <Route path="/Product" element={<Product />} />
-        <Route path="/paymentMethod" element={<PaymentMethod />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/paymentmethod" element={<PaymentMethod />} />
+        <Route path="/dashboard" element={<Dashboard />} /> 
       </Routes>
     </Router>
   );
